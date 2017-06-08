1001889323293db99bf876e8f73710598700d117
Jomar Johansen <jomarj@gmail.com>
2017-06-08 16:54:27 +0200

    Initial app structure

---
+++ Entry.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RoundLabelPrinter
+{
+    public class Entry
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+        public float Abv { get; set; }
+
+        [XmlIgnore]
+        public Image Image { get; set; }
+
+        public string ImageBase64Encoded
+        {
+            get
+            {
+                if (Image != null)
+                    return ImageToBase64(Image, System.Drawing.Imaging.ImageFormat.Png);
+                else
+                    return null;
+            }
+
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    Image = Base64ToImage(value);
+                else
+                    Image = null;
+            }
+        }
+
+        private string ImageToBase64(Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+        private Image Base64ToImage(string base64String)
+        {
+            // Convert Base64 String to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundLabelPrinter
+{
+    public class Settings
+    {
+        private static Settings _instance;
+
+        private Settings()
+        {
+            Load();
+        }
+
+        public static Settings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Settings();
+                return _instance;
+            }
+        }
+
+        private void Load()
+        {
+            _filename = LoadSetting("LastUsedFilename");
+            _previewZoom = LoadFloatSetting("PreviewZoom", 1.0f);
+        }
+
+        protected string LoadSetting(string name)
+        {
+            try
+            {
+                return Properties.Settings.Default[name] as string;
+            }
+            catch (Exception)
+            {
+
+            }
+            return string.Empty;
+        }
+
+        protected float LoadFloatSetting(string name, float defaultValue)
+        {
+            var retval = defaultValue;
+            var setting = LoadSetting(name);
+            try
+            {
+                float.TryParse(setting, out retval);
+            }
+            catch (Exception)
+            {
+            }
+            return retval;
+        }
+
+        private void Save()
+        {
+            Properties.Settings.Default["LastUsedFilename"] = _filename;
+            Properties.Settings.Default["PreviewZoom"] = _previewZoom;
+            Properties.Settings.Default.Save();
+        }
+
+        private string _filename;
+        public string LastUsedFilename
+        {
+            get { return _filename; }
+            set
+            {
+                _filename = value;
+                Save();
+            }
+        }
+
+        private float _previewZoom;
+        public float PreviewZoom
+        {
+            get { return _previewZoom; }
+            set
+            {
+                _previewZoom = value;
+                Save();
+            }
+        }
+            
+    }
+}
